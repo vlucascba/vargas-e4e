299ab28bbc662834159097dde22c32aeb614e164
Lucasvcba <lucas.e4e@gmail.com>
2024-03-27 18:13:05 -0300

    pagina ecomerce

--- public/index.js
+++ public/index.js
@@ -2,3 +2,15 @@ function toggleMenu() {
     const elementos = document.querySelector('.nav-menu');
     elementos.classList.toggle('mostrar');
 }
+
+
+/* cursos ocultar mostrar */
+
+function toggleInfo(btn) {
+    var info = btn.nextElementSibling;
+    if (info.style.display === "none") {
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+}
